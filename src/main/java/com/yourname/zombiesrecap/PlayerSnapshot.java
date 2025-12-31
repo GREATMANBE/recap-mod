f8bbe076c83a47c10ea983ef1e50f24bbbebdd0e
GREATMANBE <62797436+GREATMANBE@users.noreply.github.com>
2025-12-31 20:56:41 +0000

    Create PlayerSnapshot.java

---
+++ src/main/java/com/yourname/zombiesrecap/PlayerSnapshot.java
@@ -0,0 +1,11 @@
+package com.yourname.zombiesrecap;
+
+public class PlayerSnapshot {
+    public int kills;
+    public int gold;
+
+    public PlayerSnapshot(int kills, int gold) {
+        this.kills = kills;
+        this.gold = gold;
+    }
+}
